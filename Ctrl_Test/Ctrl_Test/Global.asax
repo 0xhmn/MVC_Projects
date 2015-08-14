592e2c1213ecb75bbd3475e0681f81f6bc6600d3
Hooman <yar00001@umn.edu>
2015-08-14 07:58:31 -0500

    changed the name of project

--- Ctrl_Test/Ctrl_Test/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ctrl_Test.MvcApplication" Language="C#" %>
--- Ctrl_Test/Ctrl_Test/Models/ViewModels/Interfaces/IEverythingShouldHave.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ctrl_Test.Models.ViewModels.Interfaces
-{
-	// make the T covarient
-	public interface IEverythingShouldHave<out TModel>
-	{
-		TModel PopulateFromApplication(string db, int id);
-		TModel PersistByApplication(string db, int id);
-	}
-}
